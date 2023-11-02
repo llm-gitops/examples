7d0f5a93a49f05dc5e2b05a07fdc5168bb931947
Chanwit Kaewkasi <chanwit@gmail.com>
2023-11-02 20:27:14 +0700

    Update main.py

--- build-run-serve-1/serve/main.py
+++ build-run-serve-1/serve/main.py
@@ -1,15 +1,18 @@
 import streamlit as st
 from streamlit.logger import get_logger
+from streamlit_autorefresh import st_autorefresh
 
 LOGGER = get_logger(__name__)
 
 def run():
+    count = st_autorefresh(interval=2000, limit=None, key="counter")
     st.set_page_config(
         page_title="BuildRunServe",
         page_icon="👋",
     )
 
     st.write("# Served by the BRS Controller 👋")
+    st.write(f"Count: {count}")
 
 if __name__ == "__main__":
     run()
