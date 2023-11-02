937aa39c1e6b0f003e36f8deeea7b4db2bf8fda8
Chanwit Kaewkasi <chanwit@gmail.com>
2023-11-02 14:27:41 +0700

    Create main.py

---
+++ build-run-serve-1/serve/main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from streamlit.logger import get_logger
+
+LOGGER = get_logger(__name__)
+
+def run():
+    st.set_page_config(
+        page_title="BuildRunServe",
+        page_icon="👋",
+    )
+
+    st.write("# Welcome to BuildRunServe Controller 👋")
+
+if __name__ == "__main__":
+    run()
